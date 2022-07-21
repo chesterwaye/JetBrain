884b0fe509791cb0afd097c30ed59307655937d5
Chester Waye <cwaye2000@gmail.com>
2022-07-20 21:43:18 -0400

    removed old code

--- honestCalculator.py
+++ honestCalculator.py
@@ -1,7 +1,4 @@
 # write your code here
-from turtle import circle
-
-
 msg_0 = "Enter an equation"
 msg_1 = "Do you even know what numbers are? Stay focused!"
 msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
