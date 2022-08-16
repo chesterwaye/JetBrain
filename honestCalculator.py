f9bfac678b134f887584cd63812de2f85ceb4597
Chester <cwaye2000@gmail.com>
2022-08-16 10:03:55 -0400

    ci: stage 5

--- honestCalculator.py
+++ honestCalculator.py
@@ -8,6 +8,13 @@ msg_6 = " ... lazy"
 msg_7 = " ... very lazy"
 msg_8 = " ... very, very lazy"
 msg_9 = "You are"
+msg_10 = "Are you sure? It is only one digit! (y / n)"
+msg_11 = "Don't be silly! It's just one number! Add to the memory? (y / n)"
+msg_11 = "Don't be silly! It's just one number! Add to the memory? (y / n)"
+msg_12 = "Last chance! Do you really want to embarrass yourself? (y / n)"
+msgs = {0: msg_0, 1: msg_1, 2: msg_2, 3: msg_3, 4: msg_4, 5: msg_5, 6: msg_6,
+        7: msg_7, 8: msg_8, 9: msg_9, 10: msg_10, 11: msg_11, 12: msg_12}
+
 
 def check(v1,v2,v3):
     msg = ""
@@ -71,7 +78,7 @@ while running:
     else:
         print(msg_2)
         continue
-    #break
+    
     print(result)
 
     answer = ""
@@ -79,7 +86,27 @@ while running:
         print(msg_4)
         answer = input()
         if answer == "y":
-            memory = result
+            #memory = result
+            if is_one_digit(result):
+                msg_index = 10
+                while True:
+                    print(msgs[msg_index])
+                    answer = input()
+                    if answer == 'y':
+                        if msg_index < 12:
+                            msg_index += 1
+                            continue
+                        else:
+                            memory = result
+                            break
+                    elif answer != 'n':
+                        continue
+                    else:
+                        break
+            else:
+                memory = result
+                
+
 
     answer = ""
     while answer != "y" and answer != "n":
